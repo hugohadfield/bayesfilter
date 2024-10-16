d895dca7544d63e5583032df4cd7816dd696d796
Hugo Hadfield <hadfield.hugo@gmail.com>
2024-10-16 20:59:57 +0100

    Try different versioning

--- bayesfilter/__init__.py
+++ bayesfilter/__init__.py
@@ -4,5 +4,5 @@ bayesfilter.
 A python library for bayesian filtering and smoothing.
 """
 
-__version__ = "0.0.4"
+from .version import __version__
 __author__ = "Hugo Hadfield"
--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
 
 from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+exec(open(os.path.join(here, 'bayesfilter/version.py')).read())
+
 
 setup(
     name='bayesfilter',
-    version='0.0.4',
+    version=__version__,
     packages=['bayesfilter'],
     install_requires=[
         'numpy',
     ],
     license='MIT',
     author='Hugo Hadfield',
+    author_email="hadfield.hugo@gmail.com",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    description="A pure Python/NumPy library for Bayesian filtering and smoothing",
+    url="https://github.com/hugohadfield/bayesfilter",
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
 )
